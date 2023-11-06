d607f63a02cfb02ad2aa74ffa0217cdb0464b223
Martin Chase <outofculture@gmail.com>
2023-11-06 14:56:42 -0800

    black

--- live_neuron_detection.py
+++ live_neuron_detection.py
@@ -11,7 +11,7 @@ from os.path import isfile, join
 import os
 import sys
 import argparse
-from yolo import YOLO#, detect_video
+from yolo import YOLO  # , detect_video
 from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
@@ -20,20 +20,22 @@ import pymmcore
 from timeit import default_timer as timer
 from datetime import datetime
 
-now = datetime.now() # datetime object containing current date and time
+now = datetime.now()  # datetime object containing current date and time
 print("now =", now)
-dt_string = now.strftime("%d/%m/%Y %H:%M:%S") # dd/mm/YY H:M:S
+dt_string = now.strftime("%d/%m/%Y %H:%M:%S")  # dd/mm/YY H:M:S
 date = now.strftime("%b-%d-%Y")
-M = 1280 # M and N are dependent on your camera pixel resolution
+M = 1280  # M and N are dependent on your camera pixel resolution
 N = 1024
 
-save_path = 'C:/Users/myip7/Documents/AND_Data/'
+save_path = "C:/Users/myip7/Documents/AND_Data/"
 
-def detect_img(yolo,img):
+
+def detect_img(yolo, img):
     # image = Image.open(img) # Only used when opening an image from a folder/ not needed for the camera
     r_image = yolo.detect_image(img)
     # r_image.show()
     return r_image
+
 
 def contrastStretch(image, min, max):
     iI = image  # image input
@@ -41,22 +43,27 @@ def contrastStretch(image, min, max):
     maxI = max  # maxmimum intensity (input)
     minO = 0  # minimum intensity (output)
     maxO = 255  # maxmimum intensity (output)
-    iO = (iI - minI) * (((maxO - minO) / (maxI - minI)) + minO)  # image output    # for m in range(0,M):
+    iO = (iI - minI) * (
+        ((maxO - minO) / (maxI - minI)) + minO
+    )  # image output    # for m in range(0,M):
     return iO
 
+
 mmc = pymmcore.CMMCore()
-print('-----setup cam-----')
-mm_dir = 'C:/Program Files/Micro-Manager-2.0gamma/' # locate your micromanager folder
+print("-----setup cam-----")
+mm_dir = "C:/Program Files/Micro-Manager-2.0gamma/"  # locate your micromanager folder
 mmc.setDeviceAdapterSearchPaths([mm_dir])
 print(mmc.getVersionInfo())
 print(mmc.getAPIVersionInfo())
 
 use_YOLO = True
-my_yolo = YOLO() # start yolo session
+my_yolo = YOLO()  # start yolo session
 
-print('-----load cam-----')
+print("-----load cam-----")
 # print(os.path.join(mm_dir, 'MMConfig_1.cfg'))
-mmc.loadSystemConfiguration(mm_dir + 'MMConfig_QCam.cfg') # load your micromanager camera configuration file
+mmc.loadSystemConfiguration(
+    mm_dir + "MMConfig_QCam.cfg"
+)  # load your micromanager camera configuration file
 mmc.setExposure(200)
 mmc.snapImage()
 im1 = mmc.getImage()
@@ -76,11 +83,13 @@ im1 = mmc.getImage()
 #     plt.show()
 
 # print('-----runnit-----')
-timeLog = save_path + date + '_test_inference_time.txt' # check the inference time from the CNN with your system
-logFileTime = open(timeLog, mode='a')
+timeLog = (
+    save_path + date + "_test_inference_time.txt"
+)  # check the inference time from the CNN with your system
+logFileTime = open(timeLog, mode="a")
 logFileTime.write("%s" % dt_string)
 
-cv2.namedWindow('live',cv2.WINDOW_AUTOSIZE)
+cv2.namedWindow("live", cv2.WINDOW_AUTOSIZE)
 mmc.startContinuousSequenceAcquisition(1)
 while True:
     if mmc.getRemainingImageCount() > 0:
@@ -94,27 +103,31 @@ while True:
         # Run detection
         if use_YOLO:
             alter = contrastStretch(frame, min_range, max_range)
-            image = Image.fromarray(np.uint8(frame)) #frame if not using contrastStretch, else alter
+            image = Image.fromarray(
+                np.uint8(frame)
+            )  # frame if not using contrastStretch, else alter
             # image = Image.fromarray(np.uint8(cm.gist_earth(frame)))
-            output = detect_img(my_yolo,image) 
+            output = detect_img(my_yolo, image)
             # output = predict_with_yolo_head(model, frame, config, confidence=0.3, iou_threshold=0.4)
             output = np.array(output)
-            cv2.imshow('live',output)
+            cv2.imshow("live", output)
         else:
-            cv2.imshow('live', frame)
+            cv2.imshow("live", frame)
         # output_image = frame
         end = timer()
-        save_time = end-start
+        save_time = end - start
         logFileTime.write("\n%s" % save_time)
         # print("Inference time: {:.2f}s".format(end - start))
 
-    if cv2.waitKey(1) & 0xFF == ord('q'): # This break key is critical, otherwise the live image does not load
+    if cv2.waitKey(1) & 0xFF == ord(
+        "q"
+    ):  # This break key is critical, otherwise the live image does not load
         break
 # Close opencv window, MMC session, YOLOv3 session, and inference time log
 cv2.destroyAllWindows()
 mmc.stopSequenceAcquisition()
 mmc.reset()
-my_yolo.close_session() # end yolo session
+my_yolo.close_session()  # end yolo session
 logFileTime.close()
 
 # print('-----hope you enjoyed the show-----')
