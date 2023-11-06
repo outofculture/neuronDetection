adc10fdbd97ccdcf5938bd4ed011acda0e85af5f
Martin Chase <outofculture@gmail.com>
2023-11-06 14:00:49 -0800

    black

--- AND_yolo3/test_AND_drivertroubleshoot.py
+++ AND_yolo3/test_AND_drivertroubleshoot.py
@@ -18,29 +18,31 @@ from PIL import Image
 import matplotlib.pyplot as plt
 from os import listdir
 from os.path import isfile, join
-import os 
+import os
 
-def detect_img(yolo,img):
+
+def detect_img(yolo, img):
     image = Image.open(img)
     r_image = yolo.detect_image(image)
     r_image.show()
     return r_image
 
-img = 'C:/Users/myip7/Documents/GitHub/neuronDetection_YOLO/slice4j.png'
+
+img = "C:/Users/myip7/Documents/GitHub/neuronDetection_YOLO/slice4j.png"
 # Set paths here
 # root_path = 'C:/Users/myip7/Dropbox (GaTech)/Shared folders/AND_Project/FINAL_CODE/Images/'
 # save_path = 'C:/Users/myip7/Documents/AND_Data/'
 # save_time = 'C:/Users/myip7/Dropbox (GaTech)/Shared folders/AND_Project/FINAL_CODE/Evaluation/inference/'
 
 # file_list = [f for f in listdir(root_path) if isfile(join(root_path, f)) & f.endswith(".png")]
-my_yolo = YOLO() # start yolo session
+my_yolo = YOLO()  # start yolo session
 
 # for f in file_list:
-    # base = os.path.basename(f)
-    # logID = save_path + os.path.splitext(base)[0] + '.txt'
-    # net = os.path.dirname(save_path)
-    # logTime = save_time + os.path.basename(net) + '_inference_time.txt'
-annotated = detect_img(my_yolo,img)
-    # annotated.save(join(save_path,f),"png")
+# base = os.path.basename(f)
+# logID = save_path + os.path.splitext(base)[0] + '.txt'
+# net = os.path.dirname(save_path)
+# logTime = save_time + os.path.basename(net) + '_inference_time.txt'
+annotated = detect_img(my_yolo, img)
+# annotated.save(join(save_path,f),"png")
 
-my_yolo.close_session() # end yolo session
+my_yolo.close_session()  # end yolo session
